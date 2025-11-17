5151abcf537ae30a03e80df14c2cc62064e28629
ivan <snapyyy@bk.ru>
2025-11-17 11:54:56 +0300

    спринт 5

---
+++ MovieQuiz/models/QuizResultsViewModel.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct QuizResultsViewModel {
+  let title: String
+  let text: String
+  let buttonText: String
+}
+//
+//  Untitled.swift
+//  MovieQuiz
+//
+//  Created by иван  on 10.11.2025.
+//
+
---
+++ MovieQuiz/models/QuizStepViewModel.swift
@@ -0,0 +1,14 @@
+import UIKit 
+
+struct QuizStepViewModel {
+  let image: UIImage
+  let question: String
+  let questionNumber: String
+}
+//
+//  Untitled.swift
+//  MovieQuiz
+//
+//  Created by иван  on 10.11.2025.
+//
+
